008acc806b8769a41dc21872f5ed41733a415c13
Mridul Hussain <111673098+mridul46@users.noreply.github.com>
2025-09-06 18:21:29 +0530

    adding Backend live link

--- frontened/src/Api.js
+++ frontened/src/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: "https://mern-todo-app-backend-7xqu.onrender.com",
   withCredentials: true, // ✅ send/receive cookies
 });
 
