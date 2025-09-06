b035db4ecef160d9ea01d8b949775f76a6796ff4
Mridul Hussain <111673098+mridul46@users.noreply.github.com>
2025-09-06 20:04:07 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 // CORS configuration (single middleware)
 app.use(cors({
-  origin: process.env.CORS_ORIGIN ||  "http://localhost:3000"
+  origin: process.env.CORS_ORIGIN ||  "http://localhost:3000",
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
