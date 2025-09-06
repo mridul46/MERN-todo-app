5177714645f021a8417ba03d5fac15bc0bef664c
Mridul Hussain <111673098+mridul46@users.noreply.github.com>
2025-09-06 20:06:39 +0530

    Update Api.js

--- frontened/src/Api.js
+++ frontened/src/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: "https://mern-todo-app-backend-uo4k.onrender.com/api/v1",
   withCredentials: true, // ✅ send/receive cookies
 });
 
